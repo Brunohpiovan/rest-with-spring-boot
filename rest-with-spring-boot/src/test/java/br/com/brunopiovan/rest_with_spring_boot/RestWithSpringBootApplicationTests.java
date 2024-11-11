cc1a66be89875ad8f64eafafd05fca9bc7f826ca
Bruno piovan <bpiovan90@gmail.com>
2024-11-11 10:23:31 -0300

    projeto iniciado

---
+++ rest-with-spring-boot/src/test/java/br/com/brunopiovan/rest_with_spring_boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunopiovan.rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
